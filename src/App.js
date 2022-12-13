b1c4ff635bc83458f02a9ecd94e655937065e1cb
Metranohm <andrewwink@gmail.com>
2022-12-13 16:25:11 -0600

    focus button

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { useState, useEffect, useRef } from "react";
 
 export default function App() {
   const [name, setName] = useState('');
-  const renderCount = useRef(0)
+  const inputRef = useRef();
 
-  useEffect(() => {
-    renderCount.current = renderCount.current + 1
-  });
+  function focus() {
+    inputRef.current.focus()
+  }
 
   return (
     <>
-    <input value={name} onChange={e => setName(e.target.value)} />
+    <input ref={inputRef} value={name} onChange={e => setName(e.target.value)} />
     <div>My name is {name}</div>
-    <div>I rendered {renderCount.current} times</div>
+    <button onClick={focus}>Focus</button>
     </>
   )
 }
